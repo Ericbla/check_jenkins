dc789f42a39e597db3b856e25dbc091bcdaef38a
Ericbla <ericbla@gmail.com>
2012-11-30 15:32:02 +0100

    - Increase performace: Refactor API access (use only one API call with ?tree= parameter) - Add absolute time (in minutes) thresholds WARNING/CRITICAL in addition to relatives ones (in %)

--- check_jenkins_job_time.pl
+++ check_jenkins_job_time.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 #
 # Check Jenkins job build time using the JSON API
 #
@@ -11,205 +10,235 @@
 # building time (estimated by Jenkins with statistics on past builds).
 # You can select a given job with -j <job-name> option or check all jobs defined on the master Jenkins
 # specified by its url.
-
 use strict;
 use LWP::UserAgent;
 use JSON;
-use Getopt::Long qw(GetOptions HelpMessage VersionMessage :config no_ignore_case bundling);
-use Pod::Usage;
+use Getopt::Long
+  qw(GetOptions HelpMessage VersionMessage :config no_ignore_case bundling);
+use Pod::Usage qw(pod2usage);
 
 # Nagios return values
 use constant {
-  OK => 0,
-  WARNING => 1,
-  CRITICAL => 2,
-  UNKNOWN => 3,
+    OK       => 0,
+    WARNING  => 1,
+    CRITICAL => 2,
+    UNKNOWN  => 3,
 };
-
 use constant API_SUFFIX => "/api/json";
-our $VERSION = '1.4';
+our $VERSION = '1.5';
 my %args;
 my $ciMasterUrl;
 my $jobName = '(All)';
-my $warn = -1;
-my $crit = -1;
-my $status = UNKNOWN;
-my $debug = 0;
-my $timeout = 30;
+my $warn    = -1;
+my $crit    = -1;
+my $awarn   = -1;
+my $acrit   = -1;
+my $status  = UNKNOWN;
+my $debug   = 0;
+my $timeout = 10;
 
-GetOptions(\%args,
-           'version|v' => sub { VersionMessage({'-exitval' => UNKNOWN}) },
-           'help|h' => sub { HelpMessage({'-exitval' => UNKNOWN}) },
-           'man' => sub { pod2usage({'-verbose' => 2, '-exitval' => UNKNOWN}) },
-           'debug|d' => \$debug,
-           'timeout|t=i' => \$timeout,
-           'proxy=s',
-           'noproxy',
-           'noperfdata',
-           'warning|w=i' => \$warn,
-           'critical|c=i' => \$crit,
-           'job|j=s' => \$jobName) or pod2usage({'-exitval' => UNKNOWN});
+# Functions prototypes
+sub test_job(\%);
+sub print_duration($);
+sub trace(@);
 
-HelpMessage({'-msg' => 'Missing Jenkins url parameter', '-exitval' => UNKNOWN}) if scalar(@ARGV) != 1;
+# Main
+GetOptions(
+    \%args,
+    'version|v' => sub { VersionMessage( { '-exitval' => UNKNOWN } ) },
+    'help|h'    => sub { HelpMessage(    { '-exitval' => UNKNOWN } ) },
+    'man' => sub { pod2usage( { '-verbose' => 2, '-exitval' => UNKNOWN } ) },
+    'debug|d'     => \$debug,
+    'timeout|t=i' => \$timeout,
+    'proxy=s',
+    'noproxy',
+    'noperfdata',
+    'warning|w=i'    => \$warn,
+    'critical|c=i'   => \$crit,
+    'absoluteWarn=i' => \$awarn,
+    'absoluteCrit=i' => \$acrit,
+    'job|j=s'        => \$jobName
+  )
+  or pod2usage( { '-exitval' => UNKNOWN } );
+HelpMessage(
+    { '-msg' => 'Missing Jenkins url parameter', '-exitval' => UNKNOWN } )
+  if scalar(@ARGV) != 1;
 $ciMasterUrl = $ARGV[0];
 $ciMasterUrl =~ s/\/$//;
-
 my $ua = LWP::UserAgent->new();
 $ua->timeout($timeout);
-if (defined($args{proxy})) {
-    $ua->proxy('http', $args{proxy});
-} else {
-    if (! defined($args{noproxy})) {
+if ( defined( $args{proxy} ) ) {
+    $ua->proxy( 'http', $args{proxy} );
+}
+else {
+    if ( !defined( $args{noproxy} ) ) {
+
         # Use HTTP_PROXY environment variable
         $ua->env_proxy;
     }
 }
-
-if ($jobName eq '(All)') {
-    # All jobs
-    my $req = HTTP::Request->new(GET => $ciMasterUrl . API_SUFFIX);
-    trace("Get ", $ciMasterUrl . API_SUFFIX, " ...\n");
-    my $res = $ua->request($req);
-    if (!$res->is_success) {
-        print ("UNKNOWN: can't get ", $ciMasterUrl . API_SUFFIX, " ($res->{status_line})\n");
-        exit UNKNOWN;
-    }
-    my $json = new JSON;
-    my $content = {};
-    eval {
-        $content = $json->decode($res->content);
-        1;
-    } or do {
-        $@ =~ s/\n//m;
-        print ("UNKNOWN: can't parse JSON content (error $@) from master api: ", $ciMasterUrl . API_SUFFIX, "\n");
-        return UNKNOWN;
-    };
-    my $jobs = $content->{'jobs'}; # ref to array
-    my $jobs_count = scalar(@$jobs);
-    trace("Got ", $jobs_count, " jobs\n");
-    if ($jobs_count == 0) {
-        print("OK: job: All (No Job)\n");
-        exit OK;
-    }
-
-    for (my $i = 0; $i <$jobs_count; $i++) {
-        my $job_url = $jobs->[$i]{'url'};
-        $status = test_job($job_url);
-        last if ($status != OK && ! $debug);
-    }
-} else {
-    $status = test_job($ciMasterUrl . '/job/' . $jobName . API_SUFFIX);
+my $url =
+    $ciMasterUrl
+  . API_SUFFIX
+  . '?tree=jobs[name,url,buildable,lastBuild[number,building,timestamp,estimatedDuration]]';
+my $req = HTTP::Request->new( GET => $url );
+trace( "Get ", $url, " ...\n" );
+my $res = $ua->request($req);
+if ( !$res->is_success ) {
+    print( "UNKNOWN: can't get ", $url, " ($res->{status_line})\n" );
+    exit UNKNOWN;
 }
-
-if ($status == OK) {
-    print ("OK: job: ", $jobName, "\n");
+my $json    = new JSON;
+my $content = {};
+eval {
+    $content = $json->decode( $res->content );
+    1;
+  }
+  or do {
+    $@ =~ s/\n//m;
+    print( "UNKNOWN: can't parse JSON content (error $@) from master api: ",
+        $url, "\n" );
+    return UNKNOWN;
+  };
+my $jobs       = $content->{'jobs'};    # ref to array
+my $jobs_count = scalar(@$jobs);
+trace( "Got ", $jobs_count, " jobs\n" );
+if ( $jobs_count == 0 ) {
+    print("OK: job: All (No Job)\n");
+    exit OK;
+}
+for ( my $i = 0 ; $i < $jobs_count ; $i++ ) {
+    if ( $jobName eq '(All)' || $jobs->[$i]->{'name'} eq $jobName ) {
+        $status = test_job( %{ $jobs->[$i] } );
+    }
+    last if ( $status != OK && !$debug );
+}
+if ( $status == OK ) {
+    print( "OK: job: ", $jobName, "\n" );
 }
 exit $status;
 
-sub test_job {
-    my $job_url = shift;
-    $job_url =~ s/\/$//;
-    my $req = HTTP::Request->new( GET => $job_url . API_SUFFIX);
-    trace("GET ", $job_url . API_SUFFIX, " ...\n");
-    my $res = $ua->request($req);
-    if (! $res->is_success) {
-        print ("UNKNOWN: can't get job api: ", $job_url . API_SUFFIX, "\n");
-        return UNKNOWN;
+sub test_job(\%) {
+    my $job = shift;
+    if ( !defined($job) || ref($job) ne 'HASH' ) {
+        trace("test_job: No job\n");
     }
-    
-    my $json = new JSON;
-    my $content = {};
-    eval {
-        $content = $json->decode($res->content);
-        1;
-    } or do {
-        $@ =~ s/\n//m;
-        print ("UNKNOWN: can't parse JSON content (error $@) from job api: ", $job_url . API_SUFFIX, "\n");
-        return UNKNOWN;
-    };
-    my $job_name = $content->{'name'};
-    trace("Job: ", $job_name);
-    if (! $content->{'buildable'}) {
+    my $job_name = $job->{'name'};
+    my $job_url  = $job->{'url'};
+    trace( "Job: ", $job_name );
+    if ( !$job->{'buildable'} ) {
+
         # Disabled job
-        trace(", disabled\n");
+        trace(", disabled \n");
         return OK;
     }
-    if (! defined($content->{'lastBuild'})) {
+    if ( !defined( $job->{'lastBuild'} ) ) {
+
         # No build for this job
         trace(", no available build\n");
         return OK;
     }
-    my $build_number = $content->{'lastBuild'}->{'number'};
-    if (defined($content->{'lastCompletedBuild'})) {
-        if ($content->{'lastCompletedBuild'}->{'number'} == $build_number) {
-            # This last build is completed, so not running
-            trace(", build number=", $build_number, " is completed \n");
-            return OK;
-        }
-    }
-    my $build_url = $content->{'lastBuild'}->{'url'};
-    $build_url =~ s/\/$//;
-    $req = HTTP::Request->new( GET => $build_url . API_SUFFIX);
-    trace('GET ', $build_url . API_SUFFIX, " ...\n");
-    $res = $ua->request($req);
-    if (! $res->is_success) {
-        # Can't get the build API
-        print "UNKNOWN: job: ", $job_name, ", can't get build api: ", $build_url . API_SUFFIX, " for build number=", $build_number, ", url=", $job_url, "\n";
-        return UNKNOWN;
-    }
-    $json = new JSON;
-    $content = {};
-    eval {
-        $content = $json->decode($res->content);
-        1;
-    } or do {
-        $@ =~ s/\n//m;
-        print ("UNKNOWN: can't parse JSON content (error $@) from build api: ", $build_url . API_SUFFIX, "\n");
-        return UNKNOWN;
-    };
-    if (! $content->{'building'}) {
-        # This build is not currently running 
-        trace(", build number=", $build_number, " is not running \n");
+    my $build_number = $job->{'lastBuild'}->{'number'};
+    if ( !$job->{'lastBuild'}->{'building'} ) {
+
+        # This build is not currently running
+        trace( ", build number=", $build_number, " is not running \n" );
         return OK;
     }
-    my $stamp = $content->{'timestamp'};
-    my $current_stamp = time() * 1000; # in ms
-    my $duration = $current_stamp - $stamp;
-    my $usual_duration = 1000 * 60 * 20; # 20 minutes
-    if (defined($content->{'estimatedDuration'}) && $content->{'estimatedDuration'} > 0) {
-        $usual_duration = $content->{'estimatedDuration'};
+    my $stamp          = $job->{'lastBuild'}->{'timestamp'};
+    my $current_stamp  = time() * 1000;                        # in ms
+    my $duration       = $current_stamp - $stamp;
+    my $usual_duration = 1000 * 60 * 30;                       # 30 minutes
+    if ( defined( $job->{'lastBuild'}->{'estimatedDuration'} )
+        && $job->{'lastBuild'}->{'estimatedDuration'} > 0 )
+    {
+        $usual_duration = $job->{'lastBuild'}->{'estimatedDuration'};
     }
-    trace(", duration=", $duration, ", usual duration=", $usual_duration, "\n");
-    if ($crit != -1 && $duration > $usual_duration + ($usual_duration * $crit / 100)) {
-        print ('CRITICAL: job: <a href="', $job_url, '">', $job_name, "</a>, build=", $build_number, ", duration=", print_duration($duration), " exeeds critical threshold =(", $crit, "%) of usual duration=", print_duration($usual_duration), "\n");
+    trace( ", duration=", $duration, ", usual duration=",
+        $usual_duration, "\n" );
+    if (   $crit != -1
+        && $duration > $usual_duration + ( $usual_duration * $crit / 100 ) )
+    {
+        print(
+            'CRITICAL: job: <a href="',      $job_url,
+            '">',                            $job_name,
+            '</a>, build=',                  $build_number,
+            ', duration=',                   print_duration($duration),
+            ' exeeds critical threshold =(', $crit,
+            '%) of usual duration=',         print_duration($usual_duration),
+            "\n"
+        );
         return CRITICAL;
     }
-    if ($warn != -1 && $duration > $usual_duration + ($usual_duration * $warn / 100)) {
-        print ('WARNING: job: <a href="', $job_url, '">', $job_name, "</a>, build=", $build_number, ", duration=", print_duration($duration), " exeeds warning threshold =(", $warn, "%) of usual duration=", print_duration($usual_duration), "\n");
+    if ( $acrit != -1 && $duration > $acrit * 60 * 1000 ) {
+        print(
+            'CRITICAL: job: <a href="',
+            $job_url,
+            '">',
+            $job_name,
+            '</a>, build=',
+            $build_number,
+            ', duration=',
+            print_duration($duration),
+            ' exeeds critical absolute threshold =(',
+            $acrit,
+            'm)',
+            "\n"
+        );
+        return CRITICAL;
+    }
+    if (   $warn != -1
+        && $duration > $usual_duration + ( $usual_duration * $warn / 100 ) )
+    {
+        print(
+            'WARNING: job: <a href="',      $job_url,
+            '">',                           $job_name,
+            '</a>, build=',                 $build_number,
+            ', duration=',                  print_duration($duration),
+            ' exeeds warning threshold =(', $warn,
+            '%) of usual duration=',        print_duration($usual_duration),
+            "\n"
+        );
+        return WARNING;
+    }
+    if ( $awarn != -1 && $duration > $awarn * 60 * 1000 ) {
+        print(
+            'WARNING: job: <a href="',
+            $job_url,
+            '">',
+            $job_name,
+            '</a>, build=',
+            $build_number,
+            ', duration=',
+            print_duration($duration),
+            ' exeeds warning absolute threshold =(',
+            $awarn,
+            'm)',
+            "\n"
+        );
         return WARNING;
     }
     return OK;
 }
 
-sub print_duration {
+sub print_duration($) {
     my $duration = shift;
-    return 'NaN' if ! defined($duration);
-    $duration /= 1000; # in sec
-    if ($duration < 60) {
-        return sprintf('%ds', $duration);
+    return 'NaN' if !defined($duration);
+    $duration /= 1000;    # in sec
+    if ( $duration < 60 ) {
+        return sprintf( '%ds', $duration );
     }
-    if ($duration < 3600) {
-        return sprintf('%.1fm', $duration / 60);
+    if ( $duration < 3600 ) {
+        return sprintf( '%.1fm', $duration / 60 );
     }
-    return sprintf('%.1fh', $duration / 3600);
+    return sprintf( '%.1fh', $duration / 3600 );
 }
 
-sub trace {
+sub trace(@) {
     if ($debug) {
         print @_;
     }
 }
-
 __END__
 
 =head1 NAME
@@ -236,6 +265,8 @@ check_jenkins_job_time.pl [options] <jenkins-url>
          --noperfdata          do not output perdata
       -w --warning=<percent>   the percentage of usual duration for the WARNING threshold
       -c --critical=<percent>  the percentage of usual duration for the CRITICAL threshold
+         --absoluteWarn=<minutes> the duration in minutes for the WARNING threshold
+         --absoluteCrit=<minutes> the duration in minutes for the CRITICAL threshold
       -j --job=<job-name>      the name of the job to monitor (default all jobs)
 
        
@@ -283,6 +314,14 @@ check_jenkins_job_time.pl [options] <jenkins-url>
 
     The percentage of usual duration for the CRITICAL threshold
     
+=item B<--absoluteWarn=>minutes
+
+    The duration in minutes for the WARNING threshold
+
+=item B<--absoluteCrit=>minutes
+
+    The duration in minutes for the CRITICAL threshold
+    
 =item B<-j> B<--job=>job-name
 
     The name of the job to monitor (default all jobs)
@@ -291,10 +330,12 @@ check_jenkins_job_time.pl [options] <jenkins-url>
 
 =head1 DESCRIPTION
 
-B<check_jenkins_job_time.pl> is a Nagios plugin check that running Jenkins jobs are not taking unusual time to complete.
-You can specify one critical and/or one warning threshold express in % of the expected
-building time (estimated by Jenkins with statistics on past builds).
-You can select a given job with -j <job-name> option or check all jobs defined on the master Jenkins
-specified by its url.
+B<check_jenkins_job_time.pl> is a Nagios plugin check that running Jenkins jobs
+are not taking unusual time to complete.
+You can specify one critical and/or one warning threshold expressed in % of the
+expected building time (estimated by Jenkins with statistics on past builds)
+or expressed in absolute minutes.
+You can select a given job with -j <job-name> option or check all jobs defined
+on the master Jenkins specified by its url.
     
 =cut
