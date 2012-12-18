5339c82c344b8955ed6f66900374489ac365b7db
Ericbla <ericbla@gmail.com>
2012-12-18 11:06:47 +0100

    Use extended status line. Misc. enhancements

--- check_jenkins_slaves.pl
+++ check_jenkins_slaves.pl
@@ -19,9 +19,8 @@ use Getopt::Long
   qw(GetOptions HelpMessage VersionMessage :config no_ignore_case bundling);
 use Pod::Usage qw(pod2usage);
 use File::Spec qw(tmpdir);
+
 #use Dumpvalue;
-
-
 # Nagios return values
 use constant {
     OK       => 0,
@@ -30,11 +29,11 @@ use constant {
     UNKNOWN  => 3,
 };
 use constant {
-    API_SUFFIX      => "/api/json",
-    COMPUTER_SUFFIX => "/computer",
+    API_SUFFIX              => "/api/json",
+    COMPUTER_SUFFIX         => "/computer",
     PERSISTENCY_FILE_PREFIX => "jenkins_slaves_",
 };
-our $VERSION = '1.0';
+our $VERSION = '1.1';
 my %args;
 my $ciMasterUrl;
 my $slaveName             = '(All)';
@@ -51,9 +50,11 @@ my $total_offline_count   = 0;
 my $total_executors_count = 0;
 my $total_running_ex      = 0;
 my $status_line           = '';
+my $status_line_ex        = '';
 my $perfdata              = '';
 my $persitency_file;
 my %slaves_status;
+
 #my $dumper = Dumpvalue->new();
 
 # Functions prototypes
@@ -65,9 +66,9 @@ sub trace(@);
 # Main
 GetOptions(
     \%args,
-    'version|v' => sub { VersionMessage( { '-exitval' => UNKNOWN } ) },
-    'help|h'    => sub { HelpMessage(    { '-exitval' => UNKNOWN } ) },
-    'man' => sub { pod2usage( { '-verbose' => 2, '-exitval' => UNKNOWN } ) },
+    'version|v' => sub { VersionMessage({'-exitval' => UNKNOWN}) },
+    'help|h'    => sub { HelpMessage({'-exitval'    => UNKNOWN}) },
+    'man' => sub { pod2usage({'-verbose' => 2, '-exitval' => UNKNOWN}) },
     'debug|d'     => \$debug,
     'timeout|t=i' => \$timeout,
     'proxy=s',
@@ -80,23 +81,23 @@ GetOptions(
     'executorCrit=i' => \$ecrit,
     'slave-name|n=s' => \$slaveName
   )
-  or pod2usage( { '-exitval' => UNKNOWN } );
-HelpMessage(
-    { '-msg' => 'Missing Jenkins url parameter', '-exitval' => UNKNOWN } )
+  or pod2usage({'-exitval' => UNKNOWN});
+HelpMessage({'-msg' => 'Missing Jenkins url parameter', '-exitval' => UNKNOWN})
   if scalar(@ARGV) != 1;
 $ciMasterUrl = $ARGV[0];
 $ciMasterUrl =~ s#/$##;
 my $instance_id = $ciMasterUrl;
 $instance_id =~ s#^http.*?://##;
 $instance_id =~ tr#/ :,;#_#s;
-$persitency_file = File::Spec->tmpdir() . '/' . PERSISTENCY_FILE_PREFIX . $instance_id . '.log';
+$persitency_file =
+  File::Spec->tmpdir() . '/' . PERSISTENCY_FILE_PREFIX . $instance_id . '.log';
 my $ua = LWP::UserAgent->new();
 $ua->timeout($timeout);
-if ( defined( $args{proxy} ) ) {
-    $ua->proxy( 'http', $args{proxy} );
-}
-else {
-    if ( !defined( $args{noproxy} ) ) {
+
+if (defined($args{proxy})) {
+    $ua->proxy('http', $args{proxy});
+} else {
+    if (!defined($args{noproxy})) {
 
         # Use HTTP_PROXY environment variable
         $ua->env_proxy;
@@ -107,209 +108,253 @@ my $url =
   . COMPUTER_SUFFIX
   . API_SUFFIX
   . '?tree=computer[displayName,executors[idle],idle,offline,offlineCause[description],temporarilyOffline]';
-my $req = HTTP::Request->new( GET => $url );
-trace( "Get ", $url, " ...\n" );
+my $req = HTTP::Request->new(GET => $url);
+trace("Get ", $url, " ...\n");
 my $res = $ua->request($req);
-if ( !$res->is_success ) {
-    print( "UNKNOWN: can't get ", $url, " ($res->{status_line})\n" );
+if (!$res->is_success) {
+    print("UNKNOWN: can't get ", $url, " ($res->{status_line})\n");
     exit UNKNOWN;
 }
 my $json    = new JSON;
 my $content = {};
 eval {
-    $content = $json->decode( $res->content );
+    $content = $json->decode($res->content);
     1;
   }
   or do {
     $@ =~ s/\n//m;
-    print( "UNKNOWN: can't parse JSON content (error $@) from master api: ",
-        $url, "\n" );
+    print("UNKNOWN: can't parse JSON content (error $@) from master api: ",
+        $url, "\n");
     return UNKNOWN;
   };
-my $computers = $content->{'computer'}; # ref to array
+my $computers = $content->{'computer'};    # ref to array of slaves
 $total_computers_count = scalar(@$computers);
 trace("Got $total_computers_count slaves\n");
-if ( $total_computers_count == 0 ) {
+if ($total_computers_count == 0) {
     print("OK: No slave\n");
     exit OK;
 }
-for ( my $i = 0 ; $i < $total_computers_count ; $i++ ) {
+$status = OK;
+
+# Loop on each slave
+for (my $i = 0; $i < $total_computers_count; $i++) {
     if (   $slaveName eq '(All)'
-        || $computers->[$i]->{'displayName'} eq $slaveName )
-    {
-        $status = test_slave( %{ $computers->[$i] } );
+        || $computers->[$i]->{'displayName'} eq $slaveName) {
+        my ($stat, $line) = (test_slave(%{$computers->[$i]}));
+
+        # Save greatest value of status
+        if ($stat > $status) {
+            $status      = $stat;
+            $status_line = $line;
+        }
     }
 }
-my $active_slaves = $total_computers_count - $total_offline_count;
 
+# Retrieve old cached slaves status
 my $old_slaves_status;
 if ($compare) {
     $old_slaves_status = read_slaves_status();
+
     #$dumper->dumpValue($old_slaves_status);
 }
+
+# Save current slaves status
 save_slaves_status(%slaves_status);
 
-# Make $_warn and $_crit to represent the the absolute lower bundary of online slaves (regarding $warn and $crit %)
+# Make $_warn and $_crit to represent the the absolute lower bundary of online
+# slaves (regarding $warn and $crit %)
 my $_warn = 0;
-if ( $warn != -1 ) {
-    $_warn = ( 100 - $warn ) * $total_computers_count / 100;
+if ($warn != -1) {
+    $_warn = (100 - $warn) * $total_computers_count / 100;
 }
 my $_crit = 0;
-if ( $crit != -1 ) {
-    $_crit = ( 100 - $crit ) * $total_computers_count / 100;
+if ($crit != -1) {
+    $_crit = (100 - $crit) * $total_computers_count / 100;
 }
 
 # Make $_ewarn and $_ecrit to represent the the absolute upper bundary of
 # running executors (regarding $ewarn and $ecrit %)
 my $_ewarn = $total_executors_count;
-if ( $ewarn != -1 ) {
+if ($ewarn != -1) {
     $_ewarn = $ewarn * $total_executors_count / 100;
 }
 my $_ecrit = $total_executors_count;
-if ( $ecrit != -1 ) {
+if ($ecrit != -1) {
     $_ecrit = $ecrit * $total_executors_count / 100;
 }
-if ( !defined $args{'noperfdata'} ) {
+
+# Compute performance data
+my $active_slaves = $total_computers_count - $total_offline_count;
+if (!defined $args{'noperfdata'}) {
     $perfdata =
         "|slaves=$active_slaves;$_warn;$_crit;$total_computers_count"
       . " executors=$total_running_ex;$_ewarn;$_ecrit;$total_executors_count";
 } else {
-     $perfdata = '';
+    $perfdata = '';
 }
 
-my $status_line_ex = '';
+# Check slaves status compared to previous ones (if any)
 if (defined $old_slaves_status && scalar(keys(%{$old_slaves_status})) > 0) {
-    trace ("Processing old slaves status\n");
+    trace("Processing old slaves status\n");
     foreach my $name (keys(%slaves_status)) {
         if (exists $old_slaves_status->{$name}) {
             if ("$old_slaves_status->{$name}" ne "$slaves_status{$name}") {
                 if ("$slaves_status{$name}" eq 'offline') {
                     trace("$name: turned offline\n");
                     $status_line_ex .= "$name: turned offline\n";
-                    $status = CRITICAL;
-                    $status_line= "CRITICAL: Slave $name: turned offline";
+                    $status      = CRITICAL;
+                    $status_line = "CRITICAL: Slave $name: turned offline";
                 } else {
                     trace("$name: turned online\n");
                     $status_line_ex .= "$name: turned online\n";
                     if ($status != CRITICAL) {
-                        $status = WARNING;
-                        $status_line= "WARNING: Slave $name turned online";
+                        $status      = WARNING;
+                        $status_line = "WARNING: Slave $name turned online";
                     }
                 }
             } else {
+
                 # Unchanged status
                 trace("$name: unchanged $slaves_status{$name} status\n");
             }
             delete $old_slaves_status->{$name};
         } else {
+
             # New slave
             trace("$name: new $slaves_status{$name} slave\n");
             $status_line_ex .= "$name: new $slaves_status{$name} slave\n";
             if ($status != CRITICAL) {
-                $status = WARNING;
-                $status_line= "WARNING: New $name slave ($slaves_status{$name})";
+                $status      = WARNING;
+                $status_line =
+                  "WARNING: New $name slave ($slaves_status{$name})";
             }
         }
     }
     foreach my $name (keys(%{$old_slaves_status})) {
+
         # Old slave
         trace("$name: removed slave\n");
         $status_line_ex .= "$name: removed slave\n";
         if ($status != CRITICAL) {
-            $status = WARNING;
-            $status_line= "WARNING: Slave $name removed";
-        }  
+            $status      = WARNING;
+            $status_line = "WARNING: Slave $name removed";
+        }
     }
 }
 
-if ( $status == OK ) {
-  if ( $crit != -1
-      && ( ( $total_offline_count * 100 ) / $total_computers_count ) > $crit )
-  {
-      print(
-"CRITICAL: $total_offline_count slaves offline / $total_computers_count > $crit\%",
-          '|', $perfdata, "\n" );
-      exit CRITICAL;
-  }
-  if ( $warn != -1
-      && ( ( $total_offline_count * 100 ) / $total_computers_count ) > $warn )
-  {
-      print(
-"WARNING: $total_offline_count slaves offline / $total_computers_count > $warn\%",
-          $perfdata, "\n"
-      );
-      exit WARNING;
-  }
-  $status_line =
-    "OK: $active_slaves online slaves (over $total_computers_count slaves)";
+# Check thresholds
+if ($status == OK) {
+
+    # Check CRITICAL threshold of offline slaves
+    if ($crit != -1
+        && (($total_offline_count * 100) / $total_computers_count) > $crit) {
+        $status_line =
+          "CRITICAL: $total_offline_count slaves offline / $total_computers_count > $crit\%";
+        $status = CRITICAL;
+    }
+
+    # Check WARNING threshold of offline slaves
+    elsif ($warn != -1
+        && (($total_offline_count * 100) / $total_computers_count) > $warn) {
+        $status_line =
+          "WARNING: $total_offline_count slaves offline / $total_computers_count > $warn\%";
+        $status = WARNING;
+    }
+
+    # Check CRITICAL threshold of total used executors
+    elsif ($ecrit != -1
+        && $total_executors_count > 0
+        && ($total_running_ex * 100 / $total_executors_count) >= $ecrit) {
+        $status_line =
+          "CRITICAL: $total_running_ex / $total_executors_count running executors >= $ecrit\%";
+        $status = CRITICAL;
+    }
+
+    # Check WARNING threshold of total used executors
+    elsif ($ewarn != -1
+        && $total_executors_count > 0
+        && ($total_running_ex * 100 / $total_executors_count) >= $ewarn) {
+        $status_line =
+          "WARNING: $total_running_ex / $total_executors_count running executors >= $ewarn\%";
+        $status = WARNING;
+    }
+
+    # All is OK
+    else {
+        $status_line =
+          "OK: $active_slaves online slaves (over $total_computers_count slaves)";
+    }
 }
-print( $status_line , $perfdata, "\n" );
-print( $status_line_ex);
+
+# Print output status
+print($status_line , $perfdata, "\n", $status_line_ex);
 exit $status;
 
+# Functions
+
 sub test_slave(\%) {
-    my $slave = shift;
-    if ( !defined($slave) || ref($slave) ne 'HASH' ) {
+    my $slave     = shift;
+    my $info_line = '';
+    if (!defined($slave) || ref($slave) ne 'HASH') {
         trace("test_slave: No slave\n");
-        return UNKNOWN;
+        return (UNKNOWN, 'test_slave: internal error');
     }
     my $slave_name = $slave->{'displayName'};
     my $executors  = $slave->{'executors'};
-    trace( "Slave: ", $slave_name );
+    $info_line = "$slave_name";
     my $executors_count = 0;
     my $running_ex      = 0;
-    if ( defined $executors ) {
+    if (defined $executors) {
         $executors_count = scalar(@$executors);
         $total_executors_count += $executors_count;
-        for ( my $i = 0 ; $i < $executors_count ; $i++ ) {
+        for (my $i = 0; $i < $executors_count; $i++) {
             next if $executors->[$i]->{'idle'};
             $running_ex++;
             $total_running_ex++;
         }
     }
-    trace(", $running_ex / $executors_count executors");
-    if ( $slave->{'offline'} ) {
-        trace(", offline");
+    $info_line .= ", $running_ex/$executors_count executors";
+    if ($slave->{'offline'}) {
+        $info_line .= ", <b>OFFLINE</b>";
         $total_offline_count++;
-        $slaves_status{$slave_name}='offline';
-    }
-    else {
-        $slaves_status{$slave_name}='online';
-        if ( !$slave->{'idle'} ) {
-            trace(", currently working");
+        $slaves_status{$slave_name} = 'offline';
+    } else {
+        $slaves_status{$slave_name} = 'online';
+        if (!$slave->{'idle'}) {
+            $info_line .= ", working";
         }
     }
-    if ( $slave->{'temporarilyOffline'} ) {
-        trace(", temporarily offline");
+    if ($slave->{'temporarilyOffline'}) {
+        $info_line .= ", temp offline";
     }
-    if ( defined( $slave->{'offlineCause'} ) ) {
-        trace(", cause: ", $slave->{'offlineCause'}->{'description'});
+    if (defined($slave->{'offlineCause'})) {
+        $info_line .= " cause: $slave->{'offlineCause'}->{'description'}";
     }
-    trace("\n");
+    $info_line .= "\n";
+    trace($info_line);
+    $status_line_ex .= $info_line;
     if (   $ecrit != -1
         && $executors_count > 0
-        && ( $running_ex * 100 / $executors_count ) >= $ecrit )
-    {
-        $status_line =
-"CRITICAL: slave $slave_name has $running_ex / $executors_count running executors >= $ecrit\%";
-        return CRITICAL;
+        && ($running_ex * 100 / $executors_count) >= $ecrit) {
+        return (CRITICAL,
+            "CRITICAL: slave $slave_name has $running_ex / $executors_count running executors >= $ecrit\%"
+        );
     }
     if (   $ewarn != -1
         && $executors_count > 0
-        && ( $running_ex * 100 / $executors_count ) >= $ewarn )
-    {
-        $status_line =
-"WARNING: slave $slave_name has $running_ex / $executors_count running executors >= $ewarn\%";
-        return WARNING;
+        && ($running_ex * 100 / $executors_count) >= $ewarn) {
+        return (WARNING,
+            "WARNING: slave $slave_name has $running_ex / $executors_count running executors >= $ewarn\%"
+        );
     }
-    return OK;
+    return (OK, '');
 }
 
 sub read_slaves_status() {
     trace("read_slaves_status: from file $persitency_file\n");
     my $ref_hash = {};
     my $fh;
-    if ( !open( $fh, '<:encoding(UTF-8)', $persitency_file ) ) {
+    if (!open($fh, '<:encoding(UTF-8)', $persitency_file)) {
         trace("Can't open file $persitency_file\n");
         return $ref_hash;
     }
@@ -319,6 +364,7 @@ sub read_slaves_status() {
         next if /^$/;
         my ($key, $val) = split(/=/);
         $ref_hash->{$key} = $val;
+
         #trace(" >> $key=$val\n");
     }
     close($fh);
@@ -329,12 +375,13 @@ sub save_slaves_status(\%) {
     my $slaves = shift;
     trace("save_slaves_status: to file $persitency_file\n");
     my $fh;
-    if ( !open( $fh, '>:encoding(UTF-8)', $persitency_file ) ) {
+    if (!open($fh, '>:encoding(UTF-8)', $persitency_file)) {
         trace("Can't write file $persitency_file\n");
         return;
     }
-    foreach my $name ( keys(%{$slaves}) ) {
+    foreach my $name (keys(%{$slaves})) {
         print $fh $name, "=", $slaves->{$name}, "\n";
+
         #trace (" << $name=$slaves->{$name}\n");
     }
     close($fh);
